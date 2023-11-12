95783a25dafab0da5079d934b24a85261ef345d1
TalhaKhan2001 <115025333+TalhaKhan2001@users.noreply.github.com>
2023-11-12 19:32:48 +0500

    Add files via upload

---
+++ FastFoodSwat/FastFoodSwat-main/assets/js/alert.js
@@ -0,0 +1,9 @@
+    window.addEventListener("load", function () {
+      alert("WELCOME");
+      this.alert
+      // Additional code or actions can be added here
+      const nextMessage = "[ ****** Developed by TEAM ULTRA **BS IT EVENING 5TH**   ******* ]!\n \n Team members names\n \n\t1. Haris Khan\n\t2. Talha Khan\n3. Haris Aman\n4. Faisal Rahman\n4. Ijaz Ali\n5.Syed Qadar";
+      alert(nextMessage);
+      // Further code or actions
+      console.log("Additional actions completed.");
+    });
